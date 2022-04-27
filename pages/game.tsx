6a209d46bb3c8a5e1986f771453a83ba7474c2b4
Matthew Parkin <matthew.parkin@skybettingandgaming.com>
2022-04-27 20:31:17 +0100

    feat(game): added game page

---
+++ pages/game.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const GamePage = () => (
+    <p>owowowowow</p>
+);
+
+GamePage.displayName = "GamePage";
+
+export default GamePage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,65 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import React from "react";
 import styles from "../styles/Home.module.css";
+import GamePage from "./game";
 
 const Home: NextPage = () => {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>{"Matthew Parkin's Portfolio"}</title>
-                <meta name="description" content="My portfolio using next JS" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Welcome!</h1>
+    <React.Fragment>
+      <div className={styles.container}>
+        <Head>
+          <title>{"Matthew Parkin's Portfolio"}</title>
+          <meta name="description" content="My portfolio using next JS" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-                <p className={styles.description}>
-                    <code className={styles.code}>{"I'm Matthew Parkin"}</code>
-                </p>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome!</h1>
 
-                <p className={styles.description}>
-                    I am a developer, learning fun things and experimenting with 3d games!!!
-                </p>
+          <p className={styles.description}>
+            <code className={styles.code}>{"I'm Matthew Parkin"}</code>
+          </p>
 
-                <div className={styles.grid}>
-                    <a href="" className={styles.card}>
-                        <h2>Games &rarr;</h2>
-                        <p>I make web games using WebGL frameworks like PixiJS and Babylon JS.</p>
-                    </a>
+          <p className={styles.description}>
+            I am a developer, learning fun things and experimenting with 3d games!!!
+          </p>
 
-                    <a href="" className={styles.card}>
-                        <h2>Photography &rarr;</h2>
-                        <p>
-                            {
-                                "I'm a huge fan of photoshop and love taking photos, you might see some on this site ;)"
-                            }
-                        </p>
-                    </a>
 
-                    <a href="" className={styles.card}>
-                        <h2>Developer &rarr;</h2>
-                        <p>I predominately work in typescript and react. </p>
-                    </a>
+          <GamePage />
+          
+          <div className={styles.grid}>
+            <a href="" className={styles.card}>
+              <h2>Games &rarr;</h2>
+              <p>I make web games using WebGL frameworks like PixiJS and Babylon JS.</p>
+            </a>
 
-                    <a href="" className={styles.card}>
-                        <h2>Design &rarr;</h2>
-                        <p>I like designing: 3D, 2D and web design</p>
-                    </a>
-                </div>
-            </main>
+            <a href="" className={styles.card}>
+              <h2>Photography &rarr;</h2>
+              <p>
+                {
+                  "I'm a huge fan of photoshop and love taking photos, you might see some on this site ;)"
+                }
+              </p>
+            </a>
 
-            <footer className={styles.footer}></footer>
-        </div>
-    );
+            <a href="" className={styles.card}>
+              <h2>Developer &rarr;</h2>
+              <p>I predominately work in typescript and react. </p>
+            </a>
+
+            <a href="" className={styles.card}>
+              <h2>Design &rarr;</h2>
+              <p>I like designing: 3D, 2D and web design</p>
+            </a>
+          </div>
+        </main>
+
+        <footer className={styles.footer}></footer>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
