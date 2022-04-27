565b1c804ecfbd27fd250a62d04dd33aefb492e3
Matthew Parkin <matthew.parkin@skybettingandgaming.com>
2022-04-27 18:53:20 +0100

    feat(infrastructure): added commitizen and changed config

---
+++ commitizen.config.js
@@ -0,0 +1,51 @@
+const types = [
+    {
+        value: "feat",
+        name: "feat:     A new feature",
+    },
+    {
+        value: "fix",
+        name: "fix:      A bug fix",
+    },
+    {
+        value: "docs",
+        name: "docs:     Documentation only changes",
+    },
+    {
+        value: "style",
+        name: `style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)`,
+    },
+    {
+        value: "refactor",
+        name: "refactor: A code change that neither fixes a bug nor adds a feature",
+    },
+    {
+        value: "perf",
+        name: "perf:     A code change that improves performance",
+    },
+    {
+        value: "test",
+        name: "test:     Adding missing tests",
+    },
+    {
+        value: "chore",
+        name: "chore:    Changes to the build process",
+    },
+    {
+        value: "revert",
+        name: "revert:   Revert to a commit",
+    },
+];
+
+const scopes = ["game", "ui", "infrastructure", "documentation"].map((name) => ({ name }));
+
+module.exports = {
+    types,
+    scopes,
+    scopeOverrides: {
+        chore: [...scopes, { name: "release" }],
+    },
+    allowCustomScopes: true,
+    allowBreakingChanges: ["feat", "fix", "perf", "refactor"],
+    skipQuestions: ["body"],
+};
