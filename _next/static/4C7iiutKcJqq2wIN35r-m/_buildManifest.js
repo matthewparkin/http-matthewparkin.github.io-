f2efe15fd32bb122d021901a211547a411b63ef0
matthewparkin <43069336+matthewparkin@users.noreply.github.com>
2022-05-09 11:37:27 +0000

    Deploying to gh-pages from @ matthewparkin/matthewparkin.github.io@2d954d9f3a0a8d8806fe329a57ade227bc464050 🚀

---
+++ _next/static/4C7iiutKcJqq2wIN35r-m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/587aac24-bff3f44aa7c198c2.js","static/css/f97f0b5df14c6c28.css","static/chunks/pages/index-59c6df54d546bec2.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
